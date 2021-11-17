508b7e820f981963d4f89e333c6ab6a4c137d260
mlh19 <89103228+mlh19@users.noreply.github.com>
2021-11-17 16:10:24 -0600

    fix bugs thats caused the server not to run and complete models.

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,43 @@
-const router = require('express').Router();
-const workoutsRoutes = require('./workoutsRoutes');
+const router = require("express").Router()
+const {Workout} = require ("../../models")
+//Aggregate the duration of workouts
+router.get ("/workouts", async (req, res) => {
+const allWorkouts = await Workout.aggregate([
+    {
+        $addFields: {
+            totalDuration: {
+                $sum: "$exercises.duration"
+            }
+        }
+    }
+]) 
+    res.json(allWorkouts)
+});
+// Create a new workout
+router.post ("/workouts", async (req, res) => {
+const newWorkout = await Workout.create({})
+res.json(newWorkout)
 
-router.use('/workouts', workoutsRoutes);
+});
 
-module.exports = router;
+// Modify workouts by id
+router.put ("/workouts/:id", async (req, res) => {
+    const updateWorkout = await Workout.findByIdAndUpdate(req.params.id,
+        {$push: {exercises: req.body}}, 
+        {new: true})
+        res.json(updateWorkout)
+});
+// Get a range of workouts from the past 7 days
+router.get ("/workouts/range", async (req, res) => {
+    const allWorkouts = await Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: "$exercises.duration"
+                }
+            }
+        }
+    ]).sort({_id: -1}).limit(7)
+        res.json(allWorkouts)
+})
+module.exports = router
--- server.js
+++ server.js
@@ -1,25 +1,22 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
-const routes = require('./controllers')
-const PORT = process.env.PORT || 3001;
-const db = require("./models");
-const { Workout } = require("./models");
+
+const PORT = process.env.PORT || 3000
+
 const app = express();
 
-app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+  useNewUrlParser: true,
+  useFindAndModify: false
 });
-app.use(routes);
+
+app.use(require("./routes"));
 
 app.listen(PORT, () => {
-    console.log(`Application running on port ${PORT}!  Visit http://localhost:${PORT}`);
+  console.log(`App running on port ${PORT}!`);
 });
